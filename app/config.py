a97641d9a04c134717a6267906ec3b24cdae7d95
varriaza <varriaza@localhost>
2026-05-23 18:29:41 -0700

    chore: add config module for env vars and constants

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MERCHANT_WALLET: str = os.getenv("MERCHANT_WALLET", "0x0000000000000000000000000000000000000000")
+SKIP_PAYMENT: bool = os.getenv("SKIP_PAYMENT", "false").lower() == "true"
+
+USDC_BASE = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+CDP_FACILITATOR_URL = "https://api.cdp.coinbase.com/platform/v2/x402/facilitator"
+NETWORK = "eip155:8453"
+GRACE_WINDOW_HOURS = 2
